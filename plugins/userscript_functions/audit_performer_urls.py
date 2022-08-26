6cce65f908be7e1838a910598a9fc88ff6b77175
7dJx1qP <38586902+7dJx1qP@users.noreply.github.com>
2022-08-26 15:41:31 -0400

    add regex pattern matching for audit performer urls

--- plugins/userscript_functions/audit_performer_urls.py
+++ plugins/userscript_functions/audit_performer_urls.py
@@ -1,4 +1,7 @@
 import log
+import os
+import pathlib
+import re
 import sys
 from urllib.parse import unquote
 try:
@@ -17,24 +20,29 @@ def to_iafd_fragment(url):
 def audit_performer_urls(db: StashDatabase):
     """Check for valid iafd url format and duplicate urls"""
 
+    regexpath = os.path.join(pathlib.Path(__file__).parent.resolve(), 'performer_url_regexes.txt')
+    patterns = [re.compile(s.strip()) for s in open(regexpath, 'r').readlines()]
+
     rows = db.fetchall("""SELECT * FROM performers WHERE url IS NOT NULL AND url <> ''""")
     performers = [PerformersRow().from_sqliterow(row) for row in rows]
     log.info(f'Checking {str(len(rows))} performers with urls...')
-    performer_fragments = {}
+    site_performer_fragments = {}
     for performer in performers:
-        if 'iafd.com' in performer.url:
-            if not performer.url.startswith('https://www.iafd.com/person.rme/perfid='):
-                log.info(f'malformed url {performer.id} {performer.name} {performer.url}')
-            else:
-                fragment = to_iafd_fragment(performer.url)
-                if fragment not in performer_fragments:
-                    performer_fragments[fragment] = performer
-                else:
-                    log.info(f'dupe performer {performer.id} {performer.name} {performer_fragments[fragment].id} {performer_fragments[fragment].name}')
+        if 'iafd.com' in performer.url and not performer.url.startswith('https://www.iafd.com/person.rme/perfid='):
+            log.info(f'malformed url {performer.id} {performer.name} {performer.url}')
+        site_id = 'OTHER'
+        url = performer.url.lower().strip()
+        performer_id = url
+        for pattern in patterns:
+            m = pattern.search(url)
+            if m:
+                site_id = m.group(1)
+                performer_id = m.group(2)
+                break
+        if site_id not in site_performer_fragments:
+            site_performer_fragments[site_id] = {}
+        if performer_id not in site_performer_fragments[site_id]:
+            site_performer_fragments[site_id][performer_id] = performer
         else:
-            fragment = performer.url
-            if fragment not in performer_fragments:
-                performer_fragments[fragment] = performer
-            else:
-                log.info(f'dupe performer {performer.id} {performer.name} {performer_fragments[fragment].id} {performer_fragments[fragment].name}')
+            log.info(f'Duplicate performer url: {performer.id} {performer.name}, {site_performer_fragments[site_id][performer_id].id} {site_performer_fragments[site_id][performer_id].name}')
     log.info('Done.')
