2229e45b2e695802c9316d297e1012f35359067f
feederbox826 <144178721+feederbox826@users.noreply.github.com>
2023-09-05 17:45:33 -0400

    Update batch save to remove fingerprint queue
    As suggested by Scruffy

--- dist/public/Stash Batch Save.user.js
+++ dist/public/Stash Batch Save.user.js
@@ -2,7 +2,7 @@
 // @name        Stash Batch Save
 // @namespace   https://github.com/7dJx1qP/stash-userscripts
 // @description Adds a batch save button to scenes tagger
-// @version     0.5.2
+// @version     0.6.0
 // @author      7dJx1qP
 // @match       http://localhost:9999/*
 // @grant       unsafeWindow
@@ -30,6 +30,7 @@
 
     function run() {
         if (!running) return;
+        removeFPQueue()
         const button = buttons.pop();
         stash.setProgress((maxCount - buttons.length) / maxCount * 100);
         if (button) {
@@ -114,6 +115,39 @@
         const taggerContainer = document.querySelector('.tagger-container');
         const saveButton = getElementByXpath("//button[text()='Save']", taggerContainer);
         btn.style.display = saveButton ? 'inline-block' : 'none';
+    }
+
+    function removeFPQueue() {
+      const DBNAME = "localforage"
+      const STORENAME = "keyvaluepairs"
+      const KEYNAME = "tagger"
+      
+      const getIDBData = (transaction) => new Promise ((resolve, reject) => {
+        const result = transaction
+          .objectStore(STORENAME)
+          .get(KEYNAME)
+        result.onsuccess = event => resolve(event.target.result)
+        result.onerror = event => reject(event.target.errorCode)
+      })
+      
+      const putIDBData = (transaction, data) => new Promise ((resolve, reject) => {
+        const result = transaction
+          .objectStore(STORENAME)
+          .put(data, KEYNAME)
+        result.onsuccess = event => resolve(event.target.result)
+        result.onerror = event => reject(event.target.errorCode)
+      })
+      
+      const IDBOpenRequest = window.indexedDB.open(DBNAME)
+      IDBOpenRequest.onsuccess = async (event) => {
+        const db = event.target.result
+        const transaction = db.transaction([STORENAME], "readwrite")
+        const data = await getIDBData(transaction)
+        // remove all fingerprints in array
+        for (const arr of Object.keys(data.fingerprintQueue)) data.fingerprintQueue[arr] = []
+        // replace object
+        await putIDBData(transaction, data)
+      }
     }
 
     stash.addEventListener('tagger:mutations:searchitems', checkSaveButtonDisplay);
--- src/body/Stash Batch Save.user.js
+++ src/body/Stash Batch Save.user.js
@@ -18,6 +18,7 @@
     let sceneId = null;
 
     function run() {
+        removeFPQueue()
         if (!running) return;
         const button = buttons.pop();
         stash.setProgress((maxCount - buttons.length) / maxCount * 100);
@@ -103,6 +104,39 @@
         const taggerContainer = document.querySelector('.tagger-container');
         const saveButton = getElementByXpath("//button[text()='Save']", taggerContainer);
         btn.style.display = saveButton ? 'inline-block' : 'none';
+    }
+
+    function removeFPQueue() {
+      const DBNAME = "localforage"
+      const STORENAME = "keyvaluepairs"
+      const KEYNAME = "tagger"
+      
+      const getIDBData = (transaction) => new Promise ((resolve, reject) => {
+        const result = transaction
+          .objectStore(STORENAME)
+          .get(KEYNAME)
+        result.onsuccess = event => resolve(event.target.result)
+        result.onerror = event => reject(event.target.errorCode)
+      })
+      
+      const putIDBData = (transaction, data) => new Promise ((resolve, reject) => {
+        const result = transaction
+          .objectStore(STORENAME)
+          .put(data, KEYNAME)
+        result.onsuccess = event => resolve(event.target.result)
+        result.onerror = event => reject(event.target.errorCode)
+      })
+      
+      const IDBOpenRequest = window.indexedDB.open(DBNAME)
+      IDBOpenRequest.onsuccess = async (event) => {
+        const db = event.target.result
+        const transaction = db.transaction([STORENAME], "readwrite")
+        const data = await getIDBData(transaction)
+        // remove all fingerprints in array
+        for (const arr of Object.keys(data.fingerprintQueue)) data.fingerprintQueue[arr] = []
+        // replace object
+        await putIDBData(transaction, data)
+      }
     }
 
     stash.addEventListener('tagger:mutations:searchitems', checkSaveButtonDisplay);
